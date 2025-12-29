0d3d44b48d35e31418db341e85d721dcd6f1524d
Piyush Paul <piyushpaul605@gmail.com>
2025-12-29 13:08:54 +0530

    Fix: added debugging exception in focus

--- api/focus.py
+++ api/focus.py
@@ -1,21 +1,23 @@
 import os
 from mailer import send_email
 from generate_daily_focus import generate_daily_focus
+import traceback
 
 
 def handler(request):
-    goal = os.environ.get("GOAL")
+    try:
+        goal = os.environ.get("GOAL")
 
-    if not goal:
+        focus = generate_daily_focus(goal)
+        send_email(focus["subject"], focus["body"])
+
         return {
-            "statusCode": 500,
-            "body": "GOAL environment variable is not set"
+            "statusCode": 200,
+            "body": "Focus email sent"
         }
 
-    focus = generate_daily_focus(goal)
-    send_email(focus["subject"], focus["body"])
-
-    return {
-        "statusCode": 200,
-        "body": "Focus email sent"
-    }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": traceback.format_exc()
+        }
